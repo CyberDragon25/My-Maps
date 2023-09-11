5e9aadf44e01b5af564ddeff9376c94289412c9d
Shubham <bhattacharya.shubham123@gmail.com>
2023-09-10 19:11:33 -0500

    Made basic UI

---
+++ app/src/main/java/edu/iastate/shubham8/mymaps/MapsAdapter.kt
@@ -0,0 +1,28 @@
+package edu.iastate.shubham8.mymaps
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
+import edu.iastate.shubham8.mymaps.models.UserMap
+
+class MapsAdapter(val context: Context, val userMaps: List<UserMap>) : RecyclerView.Adapter<MapsAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(context).inflate(android.R.layout.simple_list_item_1, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val userMap = userMaps[position]
+        val textViewTitle = holder.itemView.findViewById<TextView>(android.R.id.text1)
+        textViewTitle.text = userMap.title
+    }
+
+    override fun getItemCount() = userMaps.size
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+}
